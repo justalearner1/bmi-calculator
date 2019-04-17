bf8f2bfac4df8976959522b3bb25546788b92f85
Justalearner1 <28486498+justalearner1@users.noreply.github.com>
2019-04-17 23:07:20 +0300

    Update bmi calculator.py
    Fixed problematic functions and improved bmi logic.

--- bmi calculator.py
+++ bmi calculator.py
@@ -1,40 +1,21 @@
-# def full_name = (first_name, last_name):
-#   print(f"Hi there {first_name} {last_name}")
-#   print("Let's check your bmi")
+full_name('John', 'Doe')
 
+# bmi input data
+height_m = (170/100)
+weight_kg = 70
 
-height_m = 164 / 100
-weight_kg = 89
-
+# calculate bmi
 bmi = weight_kg / (height_m ** 2)
 
-print(weight_kg)
-print(height_m)
-print(bmi)
+# print calculated bmi value
+print(f'bmi: ' "{0:.2f}".format(bmi))
+
+# bmi logic code
 if bmi < 18.5:
     print("You are underweight")
-elif bmi <= 24.9:
-    print("You have neither underweight nor overweight")
-elif bmi <= 29.9:
-    print("You are overweight")
-else:
-    print("You are obese")
-
-
-height_m = 164 / 100
-weight_kg = 60
-
-bmi = weight_kg / (height_m ** 2)
-
-print(f"Your weight is{weight_kg}")
-print(f"Your height is{height_m}")
-print("Now let's calculate your BMI")
-print(f"Your BMI is {bmi}")
-if bmi < 18.5:
-    print("You are underweight")
-elif bmi <= 24.9:
-    print("You have neither underweight nor overweight")
-elif bmi <= 29.9:
+elif bmi >= 18.5 and bmi <= 24.9:
+    print(" You are neither underweight nor overweight")
+elif bmi >= 25 and bmi <= 29.9:
     print("You are overweight")
 else:
     print("You are obese")
